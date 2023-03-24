847eba76763daf50ad3a5d34d4a812389af68471
MaxDev <dev@mail.com>
2023-03-24 12:33:47 +0300

    Add adaptive burger menu & scripts

--- source/js/modules/burgerMenu.js
+++ source/js/modules/burgerMenu.js
@@ -1,18 +1,31 @@
 // import '../modules/animationBtn';
 
 const burgerMenu = () => {
-  const menuBtn = document.getElementById('menu-btn'),
+
+  const menuBtn = document.querySelector('.menu-btn'),
         menu = document.querySelector('.header-menu');
 
   menuBtn.addEventListener('click', () => {
-    if (menuBtn.classList.contains('open')) {
+    if (menuBtn.classList.contains('menu-btn--open')) {
       menu.classList.add('header-menu--open');
+      menu.classList.remove('header-menu--close');
     } else {
+      menu.classList.add('header-menu--close');
       menu.classList.remove('header-menu--open');
     }
 
   });
 
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 900) {
+      // console.log('Up to 900px');
+      menuBtn.classList.remove('menu-btn--open');
+      menu.classList.remove('header-menu--open');
+    } else {
+      menuBtn.classList.add('menu-btn--close');
+      menu.classList.add('header-menu--close');
+    }
+  });
 
 };
 
